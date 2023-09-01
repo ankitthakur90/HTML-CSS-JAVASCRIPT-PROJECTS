8e4f6934d14167e3ee502a60c9e43526c349e108
ANKIT THAKUR <132895399+ankitthakur90@users.noreply.github.com>
2023-09-01 15:37:09 +0530

    Project-01 passenger counting
    passenger counting

---
+++ project 01/main.js
@@ -0,0 +1,29 @@
+
+
+// document.getElementByIde('count-el').innerHTML = 5;
+let saveEl = document.getElementById("save-el");
+let countPeople = document.getElementById("count-el");
+
+let count = 0 ;
+function increment(){
+    
+    count = count + 1;
+    // countPeople.innerHTML = count;
+    countPeople.textContent = count;
+}
+
+function save(){
+    if(count != 0){
+
+    let counter =count + " - ";
+    // saveEl.innerHTML += counter;
+    saveEl.textContent += counter;
+
+    // if(saveEl.click){
+    //     count = 0;
+    // }
+    countPeople.textContent = 0 ;
+    count = 0 ;
+    
+    }
+}
