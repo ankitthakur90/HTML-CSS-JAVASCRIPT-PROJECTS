f918d33c1ff6bd18472e4187f52e2585383b2e24
Ankit Thakur <2thakurankitsingh@gmail.com>
2023-09-08 14:35:24 +0530

    Complete project 05 chrome extensions

---
+++ prectice/prc.js
@@ -0,0 +1,24 @@
+/* let myPoints  = 3;
+
+function increment(){
+    myPoints += 3;
+    console.log(myPoints);
+}
+
+function decrement(){
+    myPoints -= 1;
+    console.log(myPoints);
+}
+
+increment();
+increment()
+decrement(); 
+
+*/
+
+
+
+
+const randomNumber = ()=>Math.floor(Math.random() * 6) +1;
+
+console.log(randomNumber());
--- project 01/main.js
+++ project 01/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 // document.getElementByIde('count-el').innerHTML = 5;
@@ -27,3 +28,34 @@ function save(){
     
     }
 }
+=======
+
+
+// document.getElementByIde('count-el').innerHTML = 5;
+let saveEl = document.getElementById("save-el");
+let countPeople = document.getElementById("count-el");
+
+let count = 0 ;
+function increment(){
+    
+    count = count + 1;
+    // countPeople.innerHTML = count;
+    countPeople.textContent = count;
+}
+
+function save(){
+    if(count != 0){
+
+    let counter =count + " - ";
+    // saveEl.innerHTML += counter;
+    saveEl.textContent += counter;
+
+    // if(saveEl.click){
+    //     count = 0;
+    // }
+    countPeople.textContent = 0 ;
+    count = 0 ;
+    
+    }
+}
+>>>>>>> c929a8c (first commit)
